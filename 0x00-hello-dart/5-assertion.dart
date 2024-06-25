08fbef76150de9d4967e52fe32ba8d66d511fb21
RamziHkiri <6257@holbertonstudents.com>
2024-06-25 16:08:15 +0200

    5-assertion.dart

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
  int number = int.parse(args[0]);
-number >= 80? print("You passed"): print('Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
-
+assert(number >= 80, 'The score must be bigger or equal to 80');
+  print('You Passed');
 }
