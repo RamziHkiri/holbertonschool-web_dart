9d364f28baada08bfce58534a4acd6381087b0fd
RamziHkiri <6257@holbertonstudents.com>
2024-06-27 17:58:18 +0200

    fix

--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -6,15 +6,13 @@ class Password {
   }
 
   bool isValid() {
-    if (_password.isEmpty) return false;
+    if (_password == null || _password.isEmpty) return false;
     if (_password.length >= 8 && _password.length <= 16) {
       if (_password.contains(RegExp(r'[A-Z]')) &&
           _password.contains(RegExp(r'[a-z]')) &&
           _password.contains(RegExp(r'[0-9]'))) {
         return true;
       }
-      else
-      return false;
     }
     return false;
   }
