be4a3aa0aab0f469764a2bec0ba61a1da551fb21
RamziHkiri <6257@holbertonstudents.com>
2024-06-27 18:02:11 +0200

    fix

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,42 +1,40 @@
 import '6-password.dart';
 
-class User extends Password{
-  int id = 0;
-  String name = '';
-  int age = 0;
-  double height = 0;
-  late String user_password ='';
+class User extends Password {
+  String? name;
+  int? age;
+  double? height;
+  int? id;
+  String user_password = "";
 
-  User({required int id, required String name, required int age, required double height, required String user_password}): super(password: user_password) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.height = height;
-    this.user_password = user_password;
+  User({this.id, this.name, this.age, this.height, String? user_password}) : super(password: user_password ?? '') {
+    this.user_password = user_password ?? '';
   }
 
   Map toJson() {
     return {
-      'id' : id,
-      'name': name,
-      'age': age,
-      'height': height,
-      'user_password': user_password,
+      'id': this.id,
+      'name': this.name,
+      'age': this.age,
+      'height': this.height
+      // 'user_password': this.user_password,
     };
   }
-  static User fromJson(Map<dynamic, dynamic> userJson){
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
-      id: userJson['id'],
-      name: userJson['name'],
-      age: userJson['age'],
-      height: userJson['height'],
-      user_password: userJson['user_password'],
+      name: userJson["name"],
+      age: userJson["age"],
+      height: userJson["height"],
+      id: userJson["id"],
+      user_password: userJson["user_password"] ?? '',
     );
   }
-    @override
-  String toString() {
-    final password = Password(password: this.user_password);
 
-    return 'User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height}, Password: ${password.isValid()})';
+  @override
+  String toString() {
+    final pass = Password(password: this.user_password);
+
+    return 'User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height}, Password: ${pass.isValid()})';
   }
 }
