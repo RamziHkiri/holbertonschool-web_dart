66de7e48e37d7b480875ae292fdf7aabe431a623
RamziHkiri <6257@holbertonstudents.com>
2024-07-02 11:15:08 +0200

    greet user

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,29 @@
+import '3-util.dart';
+import 'dart:convert';
+
+
+Future<String> greetUser() async {
+ try {
+    var str = await fetchUserData();
+    var user = json.decode(str);
+    return'hello ${user["username"]}' ;
+  } catch (e) {
+    print('error caught: $e');
+
+    return 'error caught: $e';
+  }
+}
+
+loginUser() async {
+ try {
+  if (await checkCredentials()) {
+    print('There is a user: true');
+    return await greetUser();
+  } else {
+    print('There is a user: fasle');
+    return 'Wrong credentials';
+  }
+} catch (e) {
+    print('error caught: $e');
+  }
+}
